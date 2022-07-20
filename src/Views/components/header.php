5f769e0f3ec715cc4038da2d4786aff994b0d59c
Dweina1920 <dina.yamanid@gmail.com>
2022-07-20 17:51:27 +0200

    header-dina

---
+++ src/Views/components/header.php
@@ -0,0 +1,21 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Releer</title>
+    <link href="C:\xampp\htdocs\releerapp\src\css\style.css" rel="stylesheet" type="text/css">
+  
+    
+</head>
+<body>
+    <header class="header">
+        <div>
+            <img src= "" alt="logo"></img>
+        </div>
+    </header>
+</body>
+</html>
---
+++ src/Views/home.php
@@ -0,0 +1,15 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/menu.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<h1 class="huno">hola</h1>
+</body>
+</html>
